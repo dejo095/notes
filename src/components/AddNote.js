a7cead16c6e52c4ddb92c258e2a9239188fbc572
Dejan Brnic <dejan.brnic@aerdata.com>
2021-05-26 15:13:52 +0200

    ADD: some small UI improvements

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -38,34 +38,38 @@ function AddNote() {
     }
 
     return (
-        <NoteDiv className="new">
-            <Textarea value={noteInput} onChange={handleChange} cols="10" rows="4" placeholder="Type to add new note"></Textarea>
+        <AddNoteDiv className="new">
+            <Textarea value={noteInput} onChange={handleChange} rows="7" placeholder="Type to add new note" />
             <div className="note-footer">
-                <small>{ noteMaxChars - Number(noteInput.length) } characters remaining</small>
+                <small><strong>{ noteMaxChars - Number(noteInput.length) }</strong> chars remaining</small>
                 <Button disabled={!valid} onClick={handleSubmit} className="save">Save</Button>
             </div>
-        </NoteDiv>
+        </AddNoteDiv>
     )
 }
 
 export default AddNote
 
-const NoteDiv = styled.div`
+const AddNoteDiv = styled.div`
     background-color: #FFC153;
     border-radius: 10px;
-    padding: 1rem;
-    min-height:100px;
-    max-height: 200px;
+    padding: 6px;
+    height: 100%;
+    min-height: 160px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     white-space: pre-wrap;
+    box-sizing: border-box;
+    overflow-y: hidden;
 
     &.new {
         background-color: #47B39D;
     }
     
     .note-footer {
+        color: #505050;
+        padding-left: 10px;
         display: flex;
         align-items: center;
         justify-content: space-between;        
@@ -76,12 +80,12 @@ const Button = styled.button`
     background-color: #e1e1e1d2;
     border: none;
     border-radius: 15px;
-    padding: 5px 10px 5px 10px;
+    padding: 5px 10px;
 
     &:hover {
         background-color: #ffffff;
         cursor: pointer;
-        transform: scale(1.2);
+        transform: scale(1.1);
         transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
     }
 `
